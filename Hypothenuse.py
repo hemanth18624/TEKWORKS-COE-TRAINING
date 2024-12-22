36b7d4f9bd6dc75b2566354fea7087ccc929e068
Hemanth <123809089+hemanth18624@users.noreply.github.com>
2024-12-22 16:17:41 +0530

    Add files via upload

---
+++ Hypothenuse.py
@@ -0,0 +1,5 @@
+import math
+side1 = float(input("Enter the size of non hypothenuse side1 : "))
+side2 = float(input("Enter the size of non hypothenuse side1 : "))
+hypothenuse = math.sqrt(math.pow(side1,2) + math.pow(side2,2))
+print(hypothenuse)
